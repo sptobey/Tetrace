d4a7e8039e580d4ffaf6968cd6198994101f48a9
denzelr <ryan.denzel@colorado.edu>
2015-03-13 17:51:54 -0600

    rotations works but needs some work to smooth rotation and rotate around mouse

--- Assets/Scripts/peiceMove.cs
+++ Assets/Scripts/peiceMove.cs
@@ -8,29 +8,20 @@ public class peiceMove : MonoBehaviour
 	
 	private Vector3 screenPoint;
 	private Vector3 offset;
-	private bool keydown = false;
-	public float smooth = 2.0F;
-	public float tiltAngle = 60.0F;
+
 	public float zDragShift = -1.50F;
 
 	void Update() {
-		float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
-		Quaternion target = Quaternion.Euler(tiltAroundX, 0, 0);
-		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+		
+
 	}
 	
 	void OnMouseDown()
 	{
-		keydown = true;
+
 		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
 		
 		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z - zDragShift));
-		if (Input.GetKeyDown(KeyCode.A)){
-			float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
-			Quaternion target = Quaternion.Euler(tiltAroundZ, 0, 0);
-			transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
-		}
-
 	}
 	
 	void OnMouseDrag()
@@ -44,7 +35,6 @@ public class peiceMove : MonoBehaviour
 
 	void OnMouseUp()
 	{
-		keydown = false;
 		Vector3 temp;
 		temp = new Vector3(Mathf.Round(transform.position.x),Mathf.Round(transform.position.y),transform.position.z - zDragShift);
 		transform.position = temp;
--- Assets/Scripts/pieceRotate.cs
+++ Assets/Scripts/pieceRotate.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class pieceRotate : MonoBehaviour {
 	public string left;
 	public string right;
-	public string click;
+	//public string click;
+	private bool keydown = false;
+	private float startAngle = 0.0f;
+	private float tiltAngle = 90.0f;
+	public float smooth = 2.0F;
 
 	// Use this for initialization
 	void Start () {
@@ -13,15 +17,20 @@ public class pieceRotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton(click)) {
-						if (Input.GetButton (left)) {
 
-						}
-						if (Input.GetButton (right)) {
-
-						}
-				}
-	
+		if (Input.GetKeyDown (KeyCode.A) && keydown == true) {
+			transform.Rotate (0, 0, 90f);
+		}
+		if (Input.GetKeyDown (KeyCode.D) && keydown == true) {
+			transform.Rotate (0, 0, -90f);
+		}
+	}
+	 
+	void OnMouseDown(){
+		keydown = true;
 	}
 
+	void OnMouseUp(){
+		keydown = false;
+	}
 }
