f9325a71da86a80365612d82a855995f08e23302
mbinsfeld <mbinsfeld@gmail.com>
2015-03-13 15:40:26 -0600

    started rotate scripts

---
+++ Assets/Scripts/pieceRotate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pieceRotate : MonoBehaviour {
+	public string left;
+	public string right;
+	public string click;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton(click)) {
+						if (Input.GetButton (left)) {
+
+						}
+						if (Input.GetButton (right)) {
+
+						}
+				}
+	
+	}
+
+}
