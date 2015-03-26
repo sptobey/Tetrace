7c57c55fedf007d110a4053c826b60aa54c95446
denzelr <ryan.denzel@colorado.edu>
2015-03-26 14:19:40 -0600

    Smooth rotation implemented, needs pivot point fix

--- Assets/Scripts/pieceRotate.cs
+++ Assets/Scripts/pieceRotate.cs
@@ -6,23 +6,26 @@ public class pieceRotate : MonoBehaviour {
 	public string right;
 	//public string click;
 	private bool keydown = false;
-	private float startAngle = 0.0f;
-	private float tiltAngle = 90.0f;
-	public float smooth = 2.0F;
+	public float totalRotation = 0;
+	public float rotationAmount = 90f;
+	public int temp = 0;
+
+
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		if (Input.GetKeyDown (KeyCode.A) && keydown == true) {
-			transform.Rotate (0, 0, 90f);
+			//transform.Rotate (0, 0, 90F*Time.fixedDeltaTime);
+			StartCoroutine(Rotate(Vector3.forward*90, 1));
 		}
 		if (Input.GetKeyDown (KeyCode.D) && keydown == true) {
-			transform.Rotate (0, 0, -90f);
+			//transform.Rotate (0, 0, -90F*Time.fixedDeltaTime);
+			StartCoroutine(Rotate(Vector3.back*90, 1));
 		}
 	}
 	 
@@ -33,4 +36,25 @@ public class pieceRotate : MonoBehaviour {
 	void OnMouseUp(){
 		keydown = false;
 	}
+
+	IEnumerator Rotate(Vector3 byAngles, float inTime)
+	{
+		var angle = transform.eulerAngles;
+		Quaternion fromAngle = transform.rotation ;
+		Quaternion toAngle = Quaternion.Euler(transform.eulerAngles + byAngles) ;
+		for(float t = 0f ; t < 1f ; t += Time.deltaTime/inTime * 3)
+		{
+			transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t) ;
+			angle = transform.eulerAngles;
+			yield return null ;
+		}
+
+		//angle.x = Mathf.Round(angle.x / 90) * 90;
+		//angle.y = Mathf.Round(angle.y / 90) * 90;
+		angle.z = Mathf.Round(angle.z / 90) * 90;
+
+		transform.eulerAngles = angle;
+
+	}
+
 }
