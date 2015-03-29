e89bf3ca3885acbe18fc89cfc317c638df53caa4
samwise3 <samueltobey@gmail.com>
2015-03-29 10:09:09 -0600

    Work on Game Board.  Added rigid bodies to tetris pieces.  Grid piece collisions work.

---
+++ Assets/Scripts/gameGrid.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class gameGrid : MonoBehaviour {
+
+	private Dictionary<Transform, bool> gameBoard = new Dictionary<Transform, bool>();
+	private bool isFilled = false;
+
+	void Start () {
+		gameBoard.Add(transform, false);
+		foreach(Transform child in transform) {
+			gameBoard.Add(child, false);
+		}
+	}
+
+	void Update () {
+	
+	}
+	
+	public void gridWinCheck() {
+		isFilled = isGridFilled();
+		gameWin(isFilled);
+	}
+
+	private bool isGridFilled() {
+		bool filledChecker = true;
+		foreach(KeyValuePair<Transform, bool> child in gameBoard) {
+			if(child.Value == true) {
+				continue;
+			} else {
+				filledChecker = false;
+				break;
+			}
+		}
+		return filledChecker;
+	}
+
+	private void gameWin(bool boardFilled) {
+		if(boardFilled) {
+			Debug.Log("Board is Full.");
+		} else {
+			Debug.Log("Board is NOT Full");
+		}
+	}
+
+}
---
+++ Assets/Scripts/gridPiece.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gridPiece : MonoBehaviour {
+
+	private bool isOccupied;
+
+	void Start () {
+		isOccupied = false;
+	}
+	
+	void Update () {
+	
+	}
+
+	public bool getIsOccupied() {
+		return isOccupied;
+	}
+	public void setIsOccupied(bool isOccupied) {
+		this.isOccupied = isOccupied;
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log("Grid piece at (" + 
+		          transform.position.x + ", " + transform.position.y 
+		          + ") Collided with " +
+		          collision.transform.name);
+		setIsOccupied(true);
+	}
+
+	void OnCollsionExit(Collision collision) {
+		Debug.Log("Grid piece at (" + 
+		          transform.position.x + ", " + transform.position.y 
+		          + ") Stopped Colliding with " +
+		          collision.transform.name);
+		setIsOccupied(false);
+	}
+
+	/*
+	void OnCollisionStay(Collision collision) {
+		Debug.Log("Grid piece at (" + 
+		          transform.position.x + ", " + transform.position.y 
+		          + ") is Colliding with " +
+		          collision.transform.name);
+	}
+	*/
+
+}
