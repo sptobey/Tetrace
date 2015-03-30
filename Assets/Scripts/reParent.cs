f629ef75aec6b41560fd00b886f2024266b42a2a
mbinsfeld <mbinsfeld@gmail.com>
2015-03-29 22:46:27 -0600

    Trying to get rotation working better

---
+++ Assets/Scripts/reParent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reParent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown(){
+		Debug.Log ("I am alive");
+		Transform Parent = transform.parent;
+		if (transform.parent == null) {
+						Debug.Log ("I am a dummy");
+				}
+		foreach (Transform child in transform.parent) {
+			Debug.Log(child.name);
+			if(child != transform){
+				Debug.Log("good Things");
+				child.parent = gameObject.transform;
+			}
+		}
+		Parent.SetParent(gameObject.transform);
+
+	}
+}
