f82e26306b6e0b9296326d6abcb80d603f084335
denzelr <ryan.denzel@colorado.edu>
2015-04-03 15:58:59 -0600

    Added reset and main menu button to test scene, organized layout

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void MainMenu(){
+		Application.LoadLevel (0);
+	}
+	public void Reset() {
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
