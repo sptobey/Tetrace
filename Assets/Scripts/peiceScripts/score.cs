f04c493c58d0d70d23f8f01861647f1019d33e9e
shatterspike1 <stupidinternet303@gmail.com>
2015-04-22 00:25:06 -0600

    Added scoring system Score system is primitive, based on time; does nothing other than show current score yet.  Will implement screen that shows high scores, however.

---
+++ Assets/Scripts/peiceScripts/score.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class score : MonoBehaviour {
+    
+    public int scoring = 50000;
+    private IEnumerator scoredown;
+    private string s;
+    private bool scoreGot;
+    
+	// Use this for initialization
+    
+	void Start () {
+        scoreGot = false;
+	}
+    
+    void Update () {
+        if (scoring > 0) {
+            scoring = scoring - 1;
+            s = string.Concat("Score:  ", scoring.ToString());
+        }
+        if (scoreGot) {
+            killSelf();
+        }
+    }
+    
+    void getScore () {
+        scoreGot = true;
+        PlayerPrefs.SetInt("Current Score", scoring);
+        if (scoring > PlayerPrefs.GetInt("HighscoreLevel" + (Application.loadedLevel-2))) {
+            PlayerPrefs.SetInt("HighscoreLevel" + (Application.loadedLevel - 2), scoring);
+        }
+    }
+    
+    void killSelf () {
+        Destroy(this);
+    }
+    
+    
+    
+    void OnGUI() {
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), s);
+    }
+}
